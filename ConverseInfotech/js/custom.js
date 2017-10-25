4c96ae2b6adc363c1980a7edb510c554aadeaae3
harsha892 <harsha.chayanam@gmail.com>
2017-10-25 21:00:11 +0530

    converse it completed

--- ConverseInfotech/js/custom.js
+++ ConverseInfotech/js/custom.js
@@ -1,12 +1,18 @@
-  $(window).scroll(function() {
-      if ($(this).scrollTop() > 300) {
-          $('.scrollToTop').fadeIn();
-      } else {
-          $('.scrollToTop').fadeOut();
-      }
-  });
+$(window).load(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 300) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
 
-  $('.scrollToTop').on('click', function() {
-      $('html, body').animate({ scrollTop: 0 }, 800);
-      return false;
-  });
+    $('.scrollToTop').on('click', function() {
+        $('html, body').animate({ scrollTop: 0 }, 800);
+        return false;
+    });
+    var url = window.location.pathname.split('/')[2];
+    console.log(url);
+    // Will only work if string in href matches with location
+    $('ul.site-nav a[href="' + url + '"]').parent().addClass('active');
+});
